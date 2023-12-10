79efb2e9631bbe40d5ea4db5967f7df5b2491fbe
Jake Robins <me@jakerobins.com>
2023-12-10 11:32:20 -0600

    add part 2

--- puzzles/10a.go
+++ puzzles/10a.go
@@ -6,20 +6,74 @@ import (
 	"strconv"
 )
 
+type PipeContext struct {
+	Above bool
+	Below bool
+	Left bool
+	Right bool
+	PartOfLoop bool
+}
+
 type PipeMapPoint struct {
 	Char string
 	X int
 	Y int
+	Context PipeContext
 }
 
 func (p *PipeMapPoint) isSamePosition(point *PipeMapPoint) bool {
-	fmt.Println(p.X, p.Y, point.X, point.Y)
+	if (point == nil || p == nil) {
+		return false
+	}
 	return p.X == point.X && p.Y == point.Y
+}
+
+func (p *PipeMapPoint) isGround() bool {
+	return p.Char == "."
+}
+
+func (p *PipeMapPoint) isPipe() bool {
+	return !p.isGround()
+}
+
+func (p *PipeMapPoint) isInLoop() bool {
+	return p.Context.PartOfLoop
 }
 
 type PipeMap struct {
 	Grid [][]PipeMapPoint
 	StartingPositionXY *PipeMapPoint
+}
+
+
+func (p *PipeMap) GetFirstStep() *PipeMapPoint {
+	start := p.StartingPositionXY
+
+	if start.Char == "F" {
+		return p.GetRightFrom(start)
+	}
+
+	if start.Char == "7" {
+		return p.GetLeftFrom(start)
+	}
+
+	if start.Char == "|" {
+		return p.GetAboveFrom(start)
+	}
+
+	if start.Char == "-" {
+		return p.GetRightFrom(start)
+	}
+
+	if start.Char == "J" {
+		return p.GetAboveFrom(start)
+	}
+
+	if start.Char == "L" {
+		return p.GetBelowFrom(start)
+	}
+
+	return nil
 }
 
 func (p *PipeMap) SetStartPositionChar() {
@@ -86,19 +140,105 @@ func (p *PipeMap) SetStartPositionChar() {
 }
 
 func (p *PipeMap) GetLeftFrom(from *PipeMapPoint) *PipeMapPoint {
+	if (from.X == 0) {
+		return nil
+	}
 	return &p.Grid[from.Y][from.X - 1]
 }
 
 func (p *PipeMap) GetRightFrom(from *PipeMapPoint) *PipeMapPoint {
+	if (from.X == len(p.Grid[0]) - 1) {
+		return nil
+	}
 	return &p.Grid[from.Y][from.X + 1]
 }
 
 func (p *PipeMap) GetAboveFrom(from *PipeMapPoint) *PipeMapPoint {
+	if (from.Y == 0) {
+		return nil
+	}
 	return &p.Grid[from.Y - 1][from.X]
 }
 
 func (p *PipeMap) GetBelowFrom(from *PipeMapPoint) *PipeMapPoint {
+	if (from.Y == len(p.Grid) - 1) {
+		return nil
+	}
 	return &p.Grid[from.Y + 1][from.X]
+}
+
+func (p *PipeMap) SetContext(from *PipeMapPoint, current *PipeMapPoint, isLeftOnInside *bool) {
+	current.Context.PartOfLoop = true
+	
+	// We arbitrarily set Left of the pipe track as true. Later we will asign true or false as inside or outside
+
+	// came from left
+	if (p.GetLeftFrom(current).isSamePosition(from)) {
+		if current.Char == "7" {
+			current.Context.Above = true
+			current.Context.Right = true
+		}
+
+		if current.Char == "-" {
+			current.Context.Above = true
+		}
+
+		if (current.Y == len(p.Grid) - 1) {
+			*isLeftOnInside = true
+		}
+		return;
+	}
+
+	//came from above
+	if (p.GetAboveFrom(current).isSamePosition(from)) {
+		if current.Char == "J" {
+			current.Context.Right = true
+			current.Context.Below = true
+		}
+
+		if current.Char == "|" {
+			current.Context.Right = true
+		}
+
+		if (current.X == 0) {
+			*isLeftOnInside = true
+		}
+		return;
+	}
+
+	// came from right
+	if (p.GetRightFrom(current).isSamePosition(from)) {
+		if current.Char == "-" {
+			current.Context.Below = true
+		}
+
+		if current.Char == "L" {
+			current.Context.Below = true
+			current.Context.Left = true
+		}
+
+		if (current.Y == 0) {
+			*isLeftOnInside = true
+		}
+		return;
+	}
+
+	// came from below
+	if (p.GetBelowFrom(current).isSamePosition(from)) {
+		if current.Char == "|" {
+			current.Context.Left = true
+		}
+
+		if current.Char == "F" {
+			current.Context.Left = true
+			current.Context.Above = true
+		}
+
+		if (current.X == len(p.Grid[0]) - 1) {
+			*isLeftOnInside = true
+		}
+		return;
+	}
 }
 
 func (p *PipeMap) getNextPoint(from *PipeMapPoint, current *PipeMapPoint) *PipeMapPoint {
@@ -190,10 +330,12 @@ func buildPipeMap(rows []string) PipeMap {
 				Char: char,
 				X: x,
 				Y: y,
+				Context: PipeContext{},
 			}
 
 			if (char == "S") {
 				startingPositionXY = &point
+				point.Context.PartOfLoop = true
 			}
 
 			grid[y][x] = point
@@ -213,15 +355,7 @@ func Puzzle10a() string {
 	steps := 0
 	start := pipeMap.StartingPositionXY
 	from := pipeMap.StartingPositionXY
-	var compare *PipeMapPoint
-
-	if (start.Char == "F" || start.Char == "L") {
-		compare = pipeMap.GetRightFrom(start)
-	} else if (start.Char == "-" || start.Char == "J") {
-		compare = pipeMap.GetLeftFrom(start)
-	} else {
-		compare = pipeMap.GetAboveFrom(start)
-	}
+	compare := pipeMap.GetFirstStep()
 
 	for {
 		steps++
@@ -230,7 +364,6 @@ func Puzzle10a() string {
 			break
 		}
 
-		fmt.Println(from, compare)
 		nextPoint := pipeMap.getNextPoint(from, compare)
 		from = compare
 		compare = nextPoint
--- puzzles/10b.go
+++ puzzles/10b.go
@@ -2,14 +2,67 @@ package puzzles
 
 import (
 	"advent/utils"
-	"fmt"
 	"strconv"
 )
 
+
 func Puzzle10b() string {
-	data := utils.FileReader("data/10_test.txt")
+	data := utils.FileReader("data/10.txt")
+	pipeMap := buildPipeMap(data)
 
-	fmt.Println(data[0])
+	start := pipeMap.StartingPositionXY
+	from := pipeMap.StartingPositionXY
+	compare := pipeMap.GetFirstStep()
 
-	return strconv.Itoa(0)
+	var isLeftOnInside bool
+
+	// Maps routes and records all context
+	for {
+		// records which directions are inside and outside
+		pipeMap.SetContext(from, compare, &isLeftOnInside)
+		
+		if (start.isSamePosition(compare)) {
+			break
+		}
+
+		nextPoint := pipeMap.getNextPoint(from, compare)
+		from = compare
+		compare = nextPoint
+	}
+
+	count := 0
+
+	for y := 0; y < len(pipeMap.Grid); y++ {
+
+		checkPoint:
+		for x := 0; x < len(pipeMap.Grid[y]); x++ {
+			point := &pipeMap.Grid[y][x]
+
+			// ignore pipe parts in loop
+			if point.isPipe() && point.isInLoop() {
+				continue
+			}
+
+			current := point
+
+			// count left from current X until you hit a pipe or the edge
+			for pointX := point.X; pointX > 0; pointX-- {
+				compare := pipeMap.GetLeftFrom(current)
+
+				if compare.isGround() || (compare.isPipe() && !compare.isInLoop()) {
+					current = compare
+					continue
+				}
+
+				if compare.Context.Right == isLeftOnInside {
+					count++
+				}
+				
+				current = compare
+				continue checkPoint
+			}
+		}
+	}
+
+	return strconv.Itoa(count)
 }
